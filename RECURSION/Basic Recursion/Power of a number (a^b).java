0ed9c5fd0d4f63ab33c9a0326d482bbd3dac408b
Mukesh Ch <mukeshch@Mukeshs-MacBook-Air.local>
2025-06-16 19:49:24 +0530

    Power of a number

--- RECURSION/Basic Recursion/Power of a number (a^b).java
+++ RECURSION/Basic Recursion/Power of a number (a^b).java
@@ -1,12 +1,21 @@
-class Solution{
-    public int power(int a,int b){
+class Solution {
+    public double myPow(double x, int n) {
+        if(x==0 || n==1) return x;
+        if(n==0) return 1;
+        if(n<0){
+            x=1/x;
+            n=-n;
+        }
+        return helper(x,n);
+    }
+    public double helper(double a,int b){
         if(b==0) return 1;
         if(b==1) return a;
         if(b%2==0) {
-            int halfPower = power(a, b/2);
-            return halfPower * halfPower;
+            double half = helper(a, b/2);
+            return half * half;
         }
-        int halfPower = power(a, (b-1)/2);
-        return a * halfPower * halfPower;
+        double half = helper(a, b/2);
+        return a * half * half;
     }
 }
