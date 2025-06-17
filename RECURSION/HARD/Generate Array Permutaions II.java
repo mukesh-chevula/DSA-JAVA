eab0ed5ba969cc882b55e591c71942f0e4f677fa
Mukesh Chevula <145662081+mukesh-chevula@users.noreply.github.com>
2025-06-17 17:45:20 +0530

    Permutations II

--- RECURSION/HARD/Generate Array Permutaions II.java
+++ RECURSION/HARD/Generate Array Permutaions II.java
@@ -1,22 +1,27 @@
 class Solution{
-    public List<List<Integer>> permute(int[] arr) {
-        List<List<Integer>> result = new ArrayList<>();
-        generateArrayPermutationsHelper(arr, 0, result);
-        return result;
+    public List<List<Integer>> permuteUnique(int[] arr) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(arr);
+        helper(arr, 0, res);
+        return res;
     }
 
-    private void generateArrayPermutationsHelper(int[] arr, int index, List<List<Integer>> result) {
+    private void helper(int[] arr, int index, List<List<Integer>> res) {
         if (index == arr.length) {
             List<Integer> currentPermutation = new ArrayList<>();
             for (int num : arr) {
                 currentPermutation.add(num);
             }
-            result.add(currentPermutation);
+            res.add(currentPermutation);
             return;
         }
+        Set<Integer> visited = new HashSet<>();
         for (int i = index; i < arr.length; i++) {
+            if(visited.contains(arr[i])) continue;
+            visited.add(arr[i]);
+
             swap(arr, i, index);
-            generateArrayPermutationsHelper(arr, index + 1, result);
+            helper(arr, index + 1, res);
             swap(arr, i, index);
         }
     }
