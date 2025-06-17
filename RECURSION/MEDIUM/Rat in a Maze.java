32fa4a5877680d52b7970ee2d447734d0a7f318a
Mukesh Chevula <145662081+mukesh-chevula@users.noreply.github.com>
2025-06-17 22:34:47 +0530

    Rat in a Maze

--- RECURSION/MEDIUM/Rat in a Maze.java
+++ RECURSION/MEDIUM/Rat in a Maze.java
@@ -1,17 +1,15 @@
 public class Solution {
     public ArrayList<String> ratInMaze(int[][] maze) {
         int n=maze.length;
-        List<String> res = new ArrayList<>();
-        if (maze[0][0] == 0 || maze[n - 1][n - 1] == 0) {
-            return res;
-        }
+        ArrayList<String> res = new ArrayList<>();
+        if (maze[0][0] == 0 || maze[n - 1][n - 1] == 0) return res;
         boolean[][] visited = new boolean[n][n];
         solve(0, 0, maze, n, "", visited, res);
         Collections.sort(res);
         return res;
     }
 
-    private static void solve(int curx, int cury, int[][] maze, int n, String path, boolean[][] visited, List<String> res) {
+    private static void solve(int curx, int cury, int[][] maze, int n, String path, boolean[][] visited, ArrayList<String> res) {
         if (curx == n - 1 && cury == n - 1) {
             res.add(path);
             return;
@@ -21,21 +19,17 @@ public class Solution {
 
         int[] di = {1, 0, 0, -1};
         int[] dj = {0, -1, 1, 0};
-        char[] dir = {'D', 'L', 'R', 'U'};
+        String dir= new String("DLRU");
 
         for (int k = 0; k < 4; k++) {
             int newx = curx + di[k];
             int newy = cury + dj[k];
 
-            if (isSafe(newx, newy, maze, n, visited)) {
-                solve(newx, newy, maze, n, path + dir[k], visited, res);
+            if (newx >= 0 && newx < n && newy >= 0 && newy < n && maze[newx][newy] == 1 && !visited[newx][newy]) {
+                solve(newx, newy, maze, n, path + dir.charAt(k), visited, res);
             }
         }
 
         visited[curx][cury] = false;
-    }
-
-    private static boolean isSafe(int curx, int cury, int[][] maze, int n, boolean[][] visited) {
-        return curx >= 0 && curx < n && cury >= 0 && cury < n && maze[curx][cury] == 1 && !visited[curx][cury];
     }
 }
