9a675811f90100e47381fd9ae9773d3e433110af
Mukesh Chevula <145662081+mukesh-chevula@users.noreply.github.com>
2025-06-18 17:50:47 +0530

    Count Set Bits

---
+++ BIT MANIPULATION/EASY/Count Set Bits.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+class Solution {
+    public static int countSetBits(int n) {
+        // Your code here
+        int c=0;
+        while(n>1){
+            c+=n&1;
+            n=n>>1;
+        }
+        if(n==1) c++;
+        return c;
+    }
+}
