c133315c7d31a17f7a8e39e9f0c7c7d99cdc783d
Mukesh Chevula <145662081+mukesh-chevula@users.noreply.github.com>
2025-06-18 18:00:01 +0530

    Count Set Bits 2

--- BIT MANIPULATION/EASY/Count Set Bits.java
+++ BIT MANIPULATION/EASY/Count Set Bits.java
@@ -10,4 +10,12 @@ class Solution {
         if(n==1) c++;
         return c;
     }
+    public static int countSetBits(int n) {
+        int c=0;
+        while(n!-0){
+            c++;
+            n=(n&(n-1));
+        }
+        return c;
+    }
 }
