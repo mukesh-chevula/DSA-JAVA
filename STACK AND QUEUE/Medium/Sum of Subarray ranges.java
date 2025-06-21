e7aad3476018416a42c752236afc9158a3a44228
Mukesh Chevula <145662081+mukesh-chevula@users.noreply.github.com>
2025-06-21 13:21:19 +0530

    Sum of Subarray Ranges

---
+++ STACK AND QUEUE/Medium/Sum of Subarray ranges.java
@@ -0,0 +1,89 @@
+class Solution {
+    public long sumSubarrayMins(int[] arr) {
+        int length = arr.length;
+        int[] left = new int[length];
+        int[] right = new int[length];
+      
+        Arrays.fill(left, -1);
+        Arrays.fill(right, length);
+      
+        Deque<Integer> stack = new ArrayDeque<>();
+      
+        for (int i = 0; i < length; ++i) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                left[i] = stack.peek();
+            }
+            stack.push(i);
+        }
+      
+        stack.clear();
+      
+        for (int i = length - 1; i >= 0; --i) {
+            while (!stack.isEmpty() && arr[stack.peek()] > arr[i]) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                right[i] = stack.peek();
+            }
+            stack.push(i);
+        }
+      
+        int mod = (int) 1e9 + 7;
+        long answer = 0;
+      
+        for (int i = 0; i < length; ++i) {
+            long contrib = (long) (i - left[i]) * (right[i] - i) * arr[i];
+            answer += contrib;
+        }
+
+        return answer;
+    }
+    public long sumSubarrayMaxs(int[] arr) {
+        int length = arr.length;
+        int[] left = new int[length];
+        int[] right = new int[length];
+      
+        Arrays.fill(left, -1);
+        Arrays.fill(right, length);
+      
+        Deque<Integer> stack = new ArrayDeque<>();
+      
+        for (int i = 0; i < length; ++i) {
+            while (!stack.isEmpty() && arr[stack.peek()] <= arr[i]) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                left[i] = stack.peek();
+            }
+            stack.push(i);
+        }
+      
+        stack.clear();
+      
+        for (int i = length - 1; i >= 0; --i) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                stack.pop();
+            }
+            if (!stack.isEmpty()) {
+                right[i] = stack.peek();
+            }
+            stack.push(i);
+        }
+      
+        long answer = 0;
+      
+        for (int i = 0; i < length; ++i) {
+            long contrib = (long) (i - left[i]) * (right[i] - i) * arr[i];
+            answer += contrib;
+        }
+
+        return answer;
+    }
+    public long subArrayRanges(int[] nums) {
+        
+        return sumSubarrayMaxs(nums)-sumSubarrayMins(nums);
+    }
+}
