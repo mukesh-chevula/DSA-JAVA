89f38a91598c3260980b5d7dc80bd7bf2dce335a
Mukesh Chevula <145662081+mukesh-chevula@users.noreply.github.com>
2025-06-21 16:53:24 +0530

    Remove K Digits

---
+++ STACK AND QUEUE/Medium/Remove K Digits.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char digit : num.toCharArray()) {
+            while (!stack.isEmpty() && k > 0 && stack.peek() > digit) {
+                stack.pop();
+                k--;
+            }
+            stack.push(digit);
+        }
+        
+        while (k-- > 0) stack.pop();
+        
+        StringBuilder sb = new StringBuilder();
+        for (char digit : stack) sb.append(digit);
+        
+        while (sb.length() > 0 && sb.charAt(0) == '0') sb.deleteCharAt(0);
+        
+        return sb.length() > 0 ? sb.toString() : "0";
+    }
+}
