4725f95f4529f35424f8a9f82266f43f350edbb4
Mukesh CSE1 <ugs22036_cse.vardhan@cbit.org.in>
2025-08-23 18:08:39 +0530

    Minimum Insertions or Deletions to make string Palindrome

---
+++ DYNAMIC PROGRAMMING/Longest Common Subsequence/Minimum Insertions or Deletions to make string Palindrome.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minInsertions(String s) {
+        String rev = new StringBuilder(s).reverse().toString();
+
+        int a = s.length(), b = rev.length();
+        int[] prev = new int[b + 1];
+        int[] cur = new int[b + 1];
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= b; j++) {
+                if (s.charAt(i - 1) == rev.charAt(j - 1)) {
+                    cur[j] = 1 + prev[j - 1];
+                } else {
+                    cur[j] = Math.max(cur[j - 1], prev[j]);
+                }
+            }
+            prev = cur.clone();
+        }
+        return a-prev[b];
+    }
+}
